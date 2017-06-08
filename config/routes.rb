1689408d410ede1898fc02310f53071e5cfce5e3
KJAVA19811815 <karunsingh1993@gmail.com>
2017-06-08 18:17:28 +0000

    user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,12 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show, :destroy]
 
   end
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
+
+    get '/login' => 'session#new'
+    post '/login' => 'session#create'
+    get '/logout' => 'session#destroy'
 
 
 
